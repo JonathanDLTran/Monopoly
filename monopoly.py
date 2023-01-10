8fc2bd84aea03407bc5943b140c71912a0dbe468
Jonathan Tran <jtran2718@gmail.com>
2023-01-09 21:42:55 -0500

    add ccc and change:

--- monopoly.py
+++ monopoly.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import emoji
 from enum import Enum
 from random import randint, shuffle
@@ -141,8 +142,8 @@ class CommunityChest(Card):
         self.name = name
         self.action = action
 
-    def do_action(self, player, players):
-        self.action(player, players)
+    def do_action(self, player, players, properties):
+        return self.action(player, players, properties)
 
 
 class Chance(Card):
@@ -151,37 +152,360 @@ class Chance(Card):
         self.name = name
         self.action = action
 
-    def do_action(self, player, players):
-        self.action(player, players)
+    def do_action(self, player, players, properties):
+        return self.action(player, players, properties)
 
 
-CHANCE_DECK = shuffle([
-
-])
-
-
-def advance_to_go(player, players):
+def advance_to_go(player, players, properties):
+    # Do not bump up cash, as that will be handled later
     player.location = GO_ID
 
+    print("Moving to Go")
 
-def bank_error(player, players):
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def bank_error(player, players, properties):
     player.cash += 200
+    # return true to indicate card should reenter deck
+    return True
 
 
-def doctors_fees(player, players):
+def doctors_fees(player, players, properties):
+    player.cash -= 50
+    # return true to indicate card should reenter deck
+    return True
+
+
+def sell_stock(player, players, properties):
+    player.cash += 50
+    # return true to indicate card should reenter deck
+    return True
+
+
+def cc_get_out_of_jail(player, players, properties):
+    player.get_out_of_jail_cards += 1
+    player.cc_out_of_jail_cards = True
+    # return false to indicate card should NOT reenter deck
+    return False
+
+
+def go_to_jail(player, players, properties):
+    player.location = JAIL_ID
+
+    print("Moving to Jail")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def grand_opera(player, players, properties):
+    for p in players:
+        if p != player:
+            player.cash += 50
+            p.cash -= 50
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def holiday_fund(player, players, properties):
+    player.cash += 100
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def tax_refund(player, players, properties):
+    player.cash += 20
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def birthday(player, players, properties):
+    for p in players:
+        if p != player:
+            player.cash += 10
+            p.cash -= 10
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def life_insurance(player, players, properties):
+    player.cash += 100
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def hospital_fees(player, players, properties):
     player.cash -= 50
 
+    # return true to indicate card should reenter deck
+    return True
 
-def sell_stock(player, players):
-    player.cash += 50
+
+def school_fees(player, players, properties):
+    player.cash -= 50
+
+    # return true to indicate card should reenter deck
+    return True
 
 
-COMMUNITY_DECK = shuffle([
+def consultancy_fees(player, players, properties):
+    player.cash += 25
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def street_repairs(player, players, properties):
+    repair_fee = 0
+    for prop in properties:
+        if prop.owner == player.id:
+            n_houses = prop.houses
+            if 0 < n_houses <= 4:
+                repair_fee += 40
+            elif n_houses == 5:
+                repair_fee += 115
+    print(f"You owe ${repair_fee} in fees.")
+    player.cash -= repair_fee
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def second_prize(player, players, properties):
+    player.cash += 10
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def inherit(player, players, properties):
+    player.cash += 100
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+COMMUNITY_GET_OF_JAIL_CARD = CommunityChest(
+    "Get out of jail free – This card may be kept until needed or sold/traded. ", cc_get_out_of_jail)
+
+
+COMMUNITY_DECK = [
     CommunityChest("Advance To Go: Collect $200", advance_to_go),
     CommunityChest("Bank Error In Your Favor: Collect $200", bank_error),
     CommunityChest("Doctors Fees: Pay $50", doctors_fees),
-    CommunityChest("Sale of Stock: Earn $50", sell_stock),
-])
+    CommunityChest("From sale of stock you get $50", sell_stock),
+    COMMUNITY_GET_OF_JAIL_CARD,
+    CommunityChest(
+        "Go to jail. Go directly to jail. Do not pass Go, Do not collect $200. s", go_to_jail),
+    CommunityChest(
+        "Grand Opera Night. Collect $50 from every player for opening night seats.", grand_opera),
+    CommunityChest("Holiday Fund Matures: Collect $100", holiday_fund),
+    CommunityChest("Income tax refund: Collect $20", tax_refund),
+    CommunityChest(
+        "It is your birthday. Collect $10 from every player. ", birthday),
+    CommunityChest("Life insurance matures – Collect $100.", life_insurance),
+    CommunityChest("Hospital Fees. Pay $50.", hospital_fees),
+    CommunityChest("School fees. Pay $50.", school_fees),
+    CommunityChest("Receive $25 consultancy fee. ", consultancy_fees),
+    CommunityChest(
+        "You are assessed for street repairs: Pay $40 per house and $115 per hotel you own.", street_repairs),
+    CommunityChest(
+        "You have won second prize in a beauty contest. Collect $10.", second_prize),
+    CommunityChest(
+        "You inherit $100.", inherit),
+]
+shuffle(COMMUNITY_DECK)
+
+COMMUNITY_TRASH_DECK = []
+
+
+# ------------
+
+
+def illinois_ave(player, players, properties):
+    player.location = ILLINOIS_ID
+
+    print("Moving to Illinois Avenue")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def st_charles_place(player, players, properties):
+    player.location = ST_CHARLES_PLACE_ID
+
+    print("Moving to St. Charles Place")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def nearest_utility(player, players, properties):
+    location = player.location
+    differences = []
+    for i, u in enumerate(UTILITIES_LOCATIONS):
+        diff = min(abs(location - u), abs(location + NUM_PROPERTIES - u))
+        differences.append((i, diff))
+    differences = sorted(differences, key=lambda pair: pair[1])
+
+    nearest = UTILITIES_LOCATIONS[differences[0][0]]
+    player.location = nearest
+    player.pay_ten_times = True
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def nearest_railroad(player, players, properties):
+    location = player.location
+    differences = []
+    for i, u in enumerate(RAILROAD_LOCATIONS):
+        diff = min(abs(location - u), abs(location + NUM_PROPERTIES - u))
+        differences.append((i, diff))
+    differences = sorted(differences, key=lambda pair: pair[1])
+
+    nearest = RAILROAD_LOCATIONS[differences[0][0]]
+    player.location = nearest
+    player.pay_double_rent = True
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def dividend(player, players, properties):
+    player.cash += 50
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def chance_get_out_of_jail(player, players, properties):
+    player.get_out_of_jail_cards += 1
+    player.chance_out_of_jail_cards = True
+    # return false to indicate card should NOT reenter deck
+    return False
+
+
+def go_back_3_spaces(player, players, properties):
+    player.location = (player.location + NUM_PROPERTIES - 3) % NUM_PROPERTIES
+
+    print("Moving back 3 spaces")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def general_repairs(player, players, properties):
+    repair_fee = 0
+    for prop in properties:
+        if prop.owner == player.id:
+            n_houses = prop.houses
+            if 0 < n_houses <= 4:
+                repair_fee += 25
+            elif n_houses == 5:
+                repair_fee += 100
+    print(f"You owe ${repair_fee} in fees.")
+    player.cash -= repair_fee
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def reading_railroad(player, players, properties):
+    player.location = READING_RAILROAD_ID
+
+    print("Moving to Reading Railroad")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def boardwalk(player, players, properties):
+    player.location = BOARDWALK_ID
+
+    print("Moving to Boardwalk")
+
+    sleep(1)
+
+    print(build_board(players, properties))
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def chairman(player, players, properties):
+    for p in players:
+        if p != player:
+            p.cash += 50
+            player.cash -= 50
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+def matures(player, players, properties):
+    player.cash += 150
+
+    # return true to indicate card should reenter deck
+    return True
+
+
+CHANCE_GET_OF_JAIL_CARD = Chance(
+    "Get out of jail free – This card may be kept until needed or sold/traded. ", chance_get_out_of_jail)
+
+CHANCE_DECK = [
+    Chance("Advance to Go. (Collect $200)", advance_to_go),
+    Chance("Advance to Illinois Ave. If you pass Go, collect $200.", illinois_ave),
+    Chance("Advance to St. Charles Place.  If you pass Go, collect $200.",
+           st_charles_place),
+    Chance("Advance token to the nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total 10 (ten) times the amount thrown.", nearest_utility),
+    Chance("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rent to which they are otherwise entitled. If Railroad is unowned, you may buy it from the Bank.", nearest_railroad),
+    Chance("Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rent to which they are otherwise entitled. If Railroad is unowned, you may buy it from the Bank.", nearest_railroad),
+    Chance("Bank pays you dividend of $50. ", dividend),
+    CHANCE_GET_OF_JAIL_CARD,
+    Chance("Go Back Three (3) Spaces. ", go_back_3_spaces),
+    Chance("Go to Jail. Go directly to Jail. Do not pass GO, do not collect $200.", go_to_jail),
+    Chance("Make general repairs on all your property: For each house pay $25, For each hotel pay $100.", general_repairs),
+    Chance("Take a trip to Reading Railroad. If you pass Go, collect $200. ",
+           reading_railroad),
+    Chance("Take a walk on the Boardwalk. Advance token to Boardwalk. ", boardwalk),
+    Chance("You have been elected Chairman of the Board. Pay each player $50.", chairman),
+    Chance("Your building and loan matures. Receive $150. ", matures),
+]
+shuffle(CHANCE_DECK)
+
+CHANCE_TRASH_DECK = []
 
 
 class Player(object):
@@ -200,6 +524,13 @@ class Player(object):
         self.rolls_history = []
 
         self.bankrupt = False
+
+        self.chance_get_out_of_jail = False
+        self.cc_get_out_of_jail = False
+        self.get_out_of_jail_cards = 0
+
+        self.pay_double_rent = False
+        self.pay_ten_times = False
 
     def setup(self):
         name = input("Please input your name: ")
@@ -231,11 +562,10 @@ class Player(object):
         return len(self.rolls_history) >= 3 \
             and self.rolls_history[-1][0] == self.rolls_history[-1][1] \
             and self.rolls_history[-2][0] == self.rolls_history[-2][1] \
-            and self.rolls_history[-3][0] == self.rolls_history[-3][1] \
-
+            and self.rolls_history[-3][0] == self.rolls_history[-3][1]
 
     def __str__(self) -> str:
-        return f"{self.name} | {self.__symbol} | ${self.cash}"
+        return f"{self.name} | {self.__symbol} | ${self.cash} | Get Out of Jail Cards: {self.get_out_of_jail_cards}"
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -359,6 +689,10 @@ def has_monopoly(player, prop, properties):
 
 
 def one_round(players):
+    global COMMUNITY_DECK
+    global COMMUNITY_TRASH_DECK
+    global CHANCE_DECK
+    global CHANCE_TRASH_DECK
 
     for player in players:
 
@@ -380,8 +714,8 @@ def one_round(players):
         print(build_board(players, PROPERTIES))
 
         # roll dice, irregardless if player is in jail or not.
-        die1 = randint(1, 6)
-        die2 = randint(1, 6)
+        die1 = 3  # randint(1, 6)
+        die2 = 4  # randint(1, 6)
 
         # player in jail
         if player.in_jail:
@@ -396,13 +730,26 @@ def one_round(players):
                 player.cash -= JAIL_AMT
                 can_leave_jail = True
 
-            print(f"Rolling dice... {die1}, {die2}.")
+            if not can_leave_jail and player.get_out_of_jail_cards > 0:
+                resp = input("Would you like to use get out of jail card?")
+                if resp == "yes":
+                    player.get_out_of_jail_cards -= 1
+                    can_leave_jail = True
+                    if player.chance_get_out_of_jail:
+                        player.chance_get_out_of_jail = False
+                        CHANCE_TRASH_DECK.append(CHANCE_GET_OF_JAIL_CARD)
+                    else:
+                        player.cc_get_out_of_jail = False
+                        COMMUNITY_TRASH_DECK.append(COMMUNITY_GET_OF_JAIL_CARD)
 
-            if die1 == die2:
+            if not can_leave_jail:
+                print(f"Rolling dice... {die1}, {die2}.")
+
+            if not can_leave_jail and die1 == die2:
                 print("Rolled Doubles. Leaving Jail.")
                 can_leave_jail = True
 
-            if player.turns_in_jail == JAIL_MAX_TURNS:
+            if not can_leave_jail and player.turns_in_jail == JAIL_MAX_TURNS:
                 print(
                     f"Max Turns in Jail Reached. ${JAIL_AMT} charged to leave.")
                 player.cash -= JAIL_AMT
@@ -434,11 +781,42 @@ def one_round(players):
         location_key = extend_int_to_string(location)
 
         # chance card
+        if player.location in CHANCE_LOCATIONS:
+            if CHANCE_DECK == []:
+                CHANCE_DECK = deepcopy(CHANCE_TRASH_DECK)
+                shuffle(CHANCE_DECK)
+                CHANCE_TRASH_DECK = []
+
+            assert CHANCE_DECK != []
+            chance_card = CHANCE_DECK.pop()
+            print(chance_card.name)
+            action_result = chance_card.do_action(
+                player, players, PROPERTIES)
+            if action_result:
+                CHANCE_TRASH_DECK.append(chance_card)
 
         # community chest card
+        if player.location in COMMUNITY_CHEST_LOCATIONS:
+            if COMMUNITY_DECK == []:
+                COMMUNITY_DECK = deepcopy(COMMUNITY_TRASH_DECK)
+                shuffle(COMMUNITY_DECK)
+                COMMUNITY_TRASH_DECK = []
+
+            assert COMMUNITY_DECK != []
+            community_card = COMMUNITY_DECK.pop()
+            print(community_card.name)
+            action_result = community_card.do_action(
+                player, players, PROPERTIES)
+            if action_result:
+                COMMUNITY_TRASH_DECK.append(community_card)
 
         # check if enter JAIL conditions is true
         go_to_jail = False
+
+        # for chance or community chest go to jail
+        if player.location == JAIL_ID:
+            go_to_jail = True
+
         # if rolled doubles 3 times in a row, GO TO JAIL
         if player.triple_doubles():
             go_to_jail = True
@@ -448,6 +826,20 @@ def one_round(players):
         if player.location == GO_TO_JAIL_ID:
             go_to_jail = True
             print("You landed on GO TO JAIL. You are sent to jail.")
+
+        if go_to_jail:
+            resp = input("Would you like to use get out of jail card?")
+            if resp == "yes":
+                go_to_jail = False
+                player.location = JUST_VISITING_ID
+                player.get_out_of_jail_cards -= 1
+                can_leave_jail = True
+                if player.chance_get_out_of_jail:
+                    player.chance_get_out_of_jail = False
+                    CHANCE_TRASH_DECK.append(CHANCE_GET_OF_JAIL_CARD)
+                else:
+                    player.cc_get_out_of_jail = False
+                    COMMUNITY_TRASH_DECK.append(COMMUNITY_GET_OF_JAIL_CARD)
 
         if go_to_jail:
             print("You lose this turn.")
@@ -510,6 +902,20 @@ def one_round(players):
                     f"Property object {other_player_prop} type {type(other_player_prop)} not recognized. ")
 
             assert rent != None and type(rent) == int and rent > 0
+            if player.pay_double_rent:
+                print("You have to pay double rent per the chance card.")
+                rent * - 2
+                player.pay_double_rent = False
+            elif player.pay_ten_times:
+                print(
+                    "You have to pay ten times the sum of this dice roll per the chance card.")
+                roll1 = randint(1, 6)
+                roll2 = randint(1, 6)
+                print(
+                    f"Rolled {roll1}, {roll2}. You owe {10 * (roll1 + roll2)}.")
+                rent = 10 * (roll1 + roll2)
+                player.pay_ten_times = False
+
             print(f"You owe {other_player.name} ${rent} in rent.")
             other_player.cash += rent
             player.cash -= rent
@@ -666,8 +1072,34 @@ def one_round(players):
                 if location_key in CAN_OWN:
                     prop = get_property(location_key)
                     prop.owner = player.id
+            elif user_input == "community":
+                if COMMUNITY_DECK == []:
+                    COMMUNITY_DECK = deepcopy(COMMUNITY_TRASH_DECK)
+                    shuffle(COMMUNITY_DECK)
+                    COMMUNITY_TRASH_DECK = []
 
-        # Final check of player's cash balance for bankruptcy at end of turn
+                assert COMMUNITY_DECK != []
+                community_card = COMMUNITY_DECK.pop()
+                print(community_card.name)
+                action_result = community_card.do_action(
+                    player, players, PROPERTIES)
+                if action_result:
+                    COMMUNITY_TRASH_DECK.append(community_card)
+            elif user_input == "chance":
+                if CHANCE_DECK == []:
+                    CHANCE_DECK = deepcopy(CHANCE_TRASH_DECK)
+                    shuffle(CHANCE_DECK)
+                    CHANCE_TRASH_DECK = []
+
+                assert CHANCE_DECK != []
+                chance_card = CHANCE_DECK.pop()
+                print(chance_card.name)
+                action_result = chance_card.do_action(
+                    player, players, PROPERTIES)
+                if action_result:
+                    CHANCE_TRASH_DECK.append(chance_card)
+
+                # Final check of player's cash balance for bankruptcy at end of turn
         if player.cash < 0:
             print("You have become bankrupt at the end of the turn. Game over.")
             player.bankrupt = True
